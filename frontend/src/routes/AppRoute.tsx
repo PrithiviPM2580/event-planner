d79864529367d4c207ceb14140706f95505aa0d4
Prithivi <prithivi@users.noreply.github.com>
2026-04-24 21:35:22 +0545

    feat: add routes for sign up and sign in pages

--- frontend/src/routes/AppRoute.tsx
+++ frontend/src/routes/AppRoute.tsx
@@ -1,9 +1,13 @@
+import SignIn from "@/pages/auth/SignIn"
+import SignUp from "@/pages/auth/SignUp"
 import { Route, Routes } from "react-router-dom"
 
 export default function AppRoute() {
   return (
     <Routes>
       <Route path="/" element={<div>Home</div>} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/sign-in" element={<SignIn />} />
     </Routes>
   )
 }
