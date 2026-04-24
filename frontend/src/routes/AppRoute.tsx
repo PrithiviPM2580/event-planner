8cd04151e1542c4b6f77aaf84957ac021c73ac8d
Prithivi <prithivi@users.noreply.github.com>
2026-04-25 01:46:06 +0545

    feat: update routing to use Home and Dashboard components

--- frontend/src/routes/AppRoute.tsx
+++ frontend/src/routes/AppRoute.tsx
@@ -1,13 +1,16 @@
 import SignIn from "@/pages/auth/SignIn"
 import SignUp from "@/pages/auth/SignUp"
+import Dashboard from "@/pages/dashboard/Dashboard"
+import Home from "@/pages/home/Home"
 import { Route, Routes } from "react-router-dom"
 
 export default function AppRoute() {
   return (
     <Routes>
-      <Route path="/" element={<div>Home</div>} />
+      <Route path="/" element={<Home />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   )
 }
