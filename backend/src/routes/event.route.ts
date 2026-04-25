92cecf51dad9379bca1b346588ce81e44f218014
Prithivi <prithivi@users.noreply.github.com>
2026-04-25 23:58:11 +0545

    feat: implement event creation route with validation and authentication

--- backend/src/routes/event.route.ts
+++ backend/src/routes/event.route.ts
@@ -1,5 +1,17 @@
+import { createEventController } from "@/controllers/event/create-event.controller";
+import asyncHandler from "@/middleware/async-handler.middleware";
+import { requireAuth } from "@/middleware/require-auth.middleware";
+import { validateRequest } from "@/middleware/validate-request.middleware";
+import { createEventSchema } from "@/validators/event.validator";
 import { Router } from "express";
 
 const eventRouter: Router = Router();
+
+eventRouter.post(
+  "/",
+  validateRequest({ body: createEventSchema }),
+  requireAuth,
+  asyncHandler(createEventController),
+);
 
 export default eventRouter;
