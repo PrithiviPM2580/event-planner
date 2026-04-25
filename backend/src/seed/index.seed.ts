a6d4e336c853cf9758771f313c430e88cfcb3e91
Prithivi <prithivi@users.noreply.github.com>
2026-04-26 00:36:06 +0545

    feat: implement event seeding script to populate database with initial event data

---
+++ backend/src/seed/index.seed.ts
@@ -0,0 +1,42 @@
+import "dotenv/config";
+import mongoose from "mongoose";
+import { Event } from "@/model/event.model";
+import { eventSeedData } from "@/constants/index.constant";
+
+const MONGO_URI = process.env.MONGODB_URI as string;
+
+async function seedEvents() {
+  try {
+    if (!MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
+
+    // Connect DB
+    await mongoose.connect(MONGO_URI);
+    console.log("✅ MongoDB connected");
+
+    // Optional: clear old data
+    await Event.deleteMany();
+    console.log("🧹 Old events removed");
+
+    // Replace USER_ID with real ObjectId
+    const userId = new mongoose.Types.ObjectId();
+
+    const formattedData = eventSeedData.map((event) => ({
+      ...event,
+      organizerId: userId,
+    }));
+
+    // Insert data
+    await Event.insertMany(formattedData);
+
+    console.log("🌱 Events seeded successfully");
+
+    process.exit(0);
+  } catch (error) {
+    console.error("❌ Seeding failed:", error);
+    process.exit(1);
+  }
+}
+
+seedEvents();
