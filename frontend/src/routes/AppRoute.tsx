13a3bca050f11155962916ac2f2469fe6f615b71
Prithivi <prithivi@users.noreply.github.com>
2026-04-25 18:53:38 +0545

    feat: wrap Dashboard route with ProtectedRoute for access control

--- frontend/src/routes/AppRoute.tsx
+++ frontend/src/routes/AppRoute.tsx
@@ -3,6 +3,7 @@ import SignUp from "@/pages/auth/SignUp"
 import Dashboard from "@/pages/dashboard/Dashboard"
 import Home from "@/pages/home/Home"
 import { Route, Routes } from "react-router-dom"
+import ProtectedRoute from "./ProtectedRoute"
 
 export default function AppRoute() {
   return (
@@ -10,7 +11,9 @@ export default function AppRoute() {
       <Route path="/" element={<Home />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<ProtectedRoute />}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   )
 }
