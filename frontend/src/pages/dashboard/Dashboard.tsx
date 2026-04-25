fec619dc3a0bf669d0343ed9af0779f26eda49c6
Prithivi <prithivi@users.noreply.github.com>
2026-04-25 19:54:22 +0545

    refactor: replace user context with authClient session in Dashboard component

--- frontend/src/pages/dashboard/Dashboard.tsx
+++ frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,10 +1,13 @@
-import { useAuth } from "@/context/auth-context"
+import { Spinner } from "@/components/ui/spinner"
+import { authClient } from "@/lib/auth-client"
 
 export default function Dashboard() {
-  const { user } = useAuth()
-  return (
-    <div>
-      Dashboard {user?.email} :{user?.name}
-    </div>
-  )
+  const { data: session, isPending } = authClient.useSession()
+
+  if (isPending) {
+    return (
+      <Spinner className="absolute top-1/2 left-1/2 size-6 -translate-x-1/2 -translate-y-1/2" />
+    )
+  }
+  return <div>Dashboard</div>
 }
