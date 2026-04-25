ae3d26c4bfc168ef32bae147b8b005524a8351c2
Prithivi <prithivi@users.noreply.github.com>
2026-04-25 23:58:16 +0545

    feat: implement getAllEventsController for fetching public events with pagination and sorting

---
+++ backend/src/controllers/event/gell-all-events.controller.ts
@@ -0,0 +1,38 @@
+import { Event } from "@/model/event.model";
+import type { Response, NextFunction } from "express";
+import type { TypedRequest } from "@/types";
+
+export const getAllEventsController = async (
+  req: TypedRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  const sort = req.query.sort === "oldest" ? 1 : -1;
+  const filter: any = {};
+
+  filter.visibility = "PUBLIC";
+
+  if (req.query?.mine === "true") {
+    filter.organizerId = req.user?.id;
+  }
+
+  const events = await Event.find(filter)
+    .sort({ createdAt: sort })
+    .skip(skip)
+    .limit(limit)
+    .lean();
+
+  const total = await Event.countDocuments(filter);
+
+  res.status(200).json({
+    success: true,
+    events,
+    total,
+    page,
+    totalPages: Math.ceil(total / limit),
+  });
+};
