ab8c4bbf22788c10e50fc79ffdb789b2f8e0ff8e
Prithivi <prithivi@users.noreply.github.com>
2026-04-26 01:34:00 +0545

    feat: refactor event routes to include delete and update functionality

--- backend/src/routes/event.route.ts
+++ backend/src/routes/event.route.ts
@@ -1,9 +1,16 @@
 import { createEventController } from "@/controllers/event/create-event.controller";
+import { deleteEventController } from "@/controllers/event/delete-event.controller";
 import { getAllEventsController } from "@/controllers/event/gell-all-events.controller";
+import { getSingleEventController } from "@/controllers/event/get-single-event.controller";
+import { updateEventController } from "@/controllers/event/update-event.controller";
 import asyncHandler from "@/middleware/async-handler.middleware";
 import { requireAuth } from "@/middleware/require-auth.middleware";
 import { validateRequest } from "@/middleware/validate-request.middleware";
-import { createEventSchema } from "@/validators/event.validator";
+import {
+  createEventSchema,
+  eventIdParamSchema,
+  updateEventSchema,
+} from "@/validators/event.validator";
 import { Router } from "express";
 
 const eventRouter: Router = Router();
@@ -17,4 +24,23 @@ eventRouter.post(
 
 eventRouter.get("/", requireAuth, asyncHandler(getAllEventsController));
 
+eventRouter.get(
+  "/:eventId",
+  validateRequest({ params: eventIdParamSchema }),
+  requireAuth,
+  asyncHandler(getSingleEventController),
+);
+eventRouter.patch(
+  "/:eventId",
+  validateRequest({ body: updateEventSchema, params: eventIdParamSchema }),
+  requireAuth,
+  asyncHandler(updateEventController),
+);
+
+eventRouter.delete(
+  "/:eventId",
+  validateRequest({ params: eventIdParamSchema }),
+  requireAuth,
+  asyncHandler(deleteEventController),
+);
 export default eventRouter;
